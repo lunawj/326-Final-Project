a47121a2b4e5ff41b36684bbbd8536b2e4cb2bd0
lunawj <39808135+lunawj@users.noreply.github.com>
2019-11-12 17:08:19 -0500

    Add files via upload

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../driverlib_empty_project_ccs.cmd 
+
+LIB_SRCS += \
+C:/ti/msp/MSP432Ware_3_50_00_02/driverlib/driverlib/MSP432P4xx/ccs/msp432p4xx_driverlib.lib 
+
+C_SRCS += \
+../RTC.c \
+../Rotary_Encoder.c \
+../ST7735.c \
+../main.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./RTC.d \
+./Rotary_Encoder.d \
+./ST7735.d \
+./main.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./RTC.obj \
+./Rotary_Encoder.obj \
+./ST7735.obj \
+./main.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"RTC.obj" \
+"Rotary_Encoder.obj" \
+"ST7735.obj" \
+"main.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"RTC.d" \
+"Rotary_Encoder.d" \
+"ST7735.d" \
+"main.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"../RTC.c" \
+"../Rotary_Encoder.c" \
+"../ST7735.c" \
+"../main.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
